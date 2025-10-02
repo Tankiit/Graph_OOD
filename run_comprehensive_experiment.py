9ab7878e0702de36ea41ae3d2ab85ff47a0b2c61
Astrid Klipfel <astridklipfel@gmail.com>
2025-10-02 10:35:34 +0200

    fix dataset config (forgot to fix conflict)

--- run_comprehensive_experiment.py
+++ run_comprehensive_experiment.py
@@ -57,24 +57,13 @@ class ExperimentOrchestrator:
         """Define all experimental configurations"""
         
         # Dataset configurations
-<<<<<<< HEAD
         id_datasets = ['cifar10', 'cifar100', 'svhn', 'tiny_imagenet']
-=======
-        id_datasets = ['cifar10', 'tiny_imagenet']
-        ood_datasets = {
-            'cifar10': ['tiny_imagenet'],
-            'tiny_imagenet': ['cifar10'], 
-        }
-        """
-        id_datasets = ['cifar10', 'cifar100', 'svhn']
->>>>>>> 82612f0 (add tiny imagenet)
         ood_datasets = {
             'cifar10': ['cifar100', 'tiny_imagenet', 'svhn', 'noise', 'texture'],
             'cifar100': ['cifar10', 'tiny_imagenet', 'svhn', 'noise', 'texture'], 
             'tiny_imagenet': ['cifar10', 'cifar100', 'svhn', 'noise', 'texture'], 
             'svhn': ['cifar10', 'cifar100', 'tiny_imagenet', 'noise', 'texture']
         }
-        """
         
         # Architecture configurations
         architectures = {
